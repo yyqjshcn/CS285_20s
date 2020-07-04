dd219a03e39d975d02f7d03f6329812113675153
George Yao <yyqjshcn@qq.com>
2020-07-04 06:49:47 -0700

    more files

--- movingByCartesian.py
+++ movingByCartesian.py
@@ -25,6 +25,7 @@ prefix = 'NO_ROBOT_TYPE_DEFINED_'
 finger_maxDist = 18.9/2/1000  # max distance for one finger
 finger_maxTurn = 6800  # max thread rotation for one finger
 currentCartesianCommand = [0.212322831154, -0.257197618484, 0.509646713734, 1.63771402836, 1.11316478252, 0.134094119072] # default home in unit mq
+currentFingerPosition = [0.0, 0.0, 0.0]
 
 # sending material control
 # serialPort = "COM4"
@@ -156,7 +157,7 @@ def kinova_robotTypeParser(kinova_robotType_):
     finger_maxTurn = 6800  # max thread turn for one finger
 
 
-def unitParser(unit_, pose_value_, relative_):
+def unitParserCartesian(unit_, pose_value_, relative_):
     """ Argument unit """
     global currentCartesianCommand
 
@@ -214,24 +215,120 @@ def unitParser(unit_, pose_value_, relative_):
     return pose_mq_, pose_mdeg_, pose_mrad_
 
 
-def verboseParser(verbose, pose_mq_):
+def verboseParserCartesian(verbose, pose_mq_):
     """ Argument verbose """
     position_ = pose_mq_[:3]
     orientation_q = pose_mq_[3:]
     if verbose:
         orientation_rad = Quaternion2EulerXYZ(orientation_q)
         orientation_deg = list(map(math.degrees, orientation_rad))
-        # print('Cartesian position is: {}'.format(position_))
-        # print('Cartesian orientation in Quaternion is: ')
-        # print('qx {:0.3f}, qy {:0.3f}, qz {:0.3f}, qw {:0.3f}'.format(orientation_q[0], orientation_q[1], orientation_q[2], orientation_q[3]))
-        # print('Cartesian orientation in Euler-XYZ(radian) is: ')
-        # print('tx {:0.3f}, ty {:0.3f}, tz {:0.3f}'.format(orientation_rad[0], orientation_rad[1], orientation_rad[2]))
-        # print('Cartesian orientation in Euler-XYZ(degree) is: ')
-        # print('tx {:3.1f}, ty {:3.1f}, tz {:3.1f}'.format(orientation_deg[0], orientation_deg[1], orientation_deg[2]))
+        print('Cartesian position is: {}'.format(position_))
+        print('Cartesian orientation in Quaternion is: ')
+        print('qx {:0.3f}, qy {:0.3f}, qz {:0.3f}, qw {:0.3f}'.format(orientation_q[0], orientation_q[1], orientation_q[2], orientation_q[3]))
+        print('Cartesian orientation in Euler-XYZ(radian) is: ')
+        print('tx {:0.3f}, ty {:0.3f}, tz {:0.3f}'.format(orientation_rad[0], orientation_rad[1], orientation_rad[2]))
+        print('Cartesian orientation in Euler-XYZ(degree) is: ')
+        print('tx {:3.1f}, ty {:3.1f}, tz {:3.1f}'.format(orientation_deg[0], orientation_deg[1], orientation_deg[2]))
+
+# Finger controller begin here
+def gripper_client(finger_positions):
+    """Send a gripper goal to the action server."""
+    action_address = '/' + prefix + 'driver/fingers_action/finger_positions'
+
+    client = actionlib.SimpleActionClient(action_address,
+                                          kinova_msgs.msg.SetFingersPositionAction)
+    client.wait_for_server()
+
+    goal = kinova_msgs.msg.SetFingersPositionGoal()
+    goal.fingers.finger1 = float(finger_positions[0])
+    goal.fingers.finger2 = float(finger_positions[1])
+    # The MICO arm has only two fingers, but the same action definition is used
+    if len(finger_positions) < 3:
+        goal.fingers.finger3 = 0.0
+    else:
+        goal.fingers.finger3 = float(finger_positions[2])
+    client.send_goal(goal)
+    if client.wait_for_result(rospy.Duration(5.0)):
+        return client.get_result()
+    else:
+        client.cancel_all_goals()
+        rospy.logwarn('        the gripper action timed-out')
+        return None
+
+def getCurrentFingerPosition(prefix_):
+    # wait to get current position
+    topic_address = '/' + prefix_ + 'driver/out/finger_position'
+    rospy.Subscriber(topic_address, kinova_msgs.msg.FingerPosition, setCurrentFingerPosition)
+    rospy.wait_for_message(topic_address, kinova_msgs.msg.FingerPosition)
+    print 'obtained current finger position '
 
 
+def setCurrentFingerPosition(feedback):
+    global currentFingerPosition
+    currentFingerPosition[0] = feedback.finger1
+    currentFingerPosition[1] = feedback.finger2
+    currentFingerPosition[2] = feedback.finger3
+
+def unitParserFinger(unit_, finger_value_, relative_):
+    """ Argument unit """
+    global currentFingerPosition
+
+    # transform between units
+    if unit_ == 'turn':
+        # get absolute value
+        if relative_:
+            finger_turn_absolute_ = [finger_value_[i] + currentFingerPosition[i] for i in range(0, len(finger_value_))]
+        else:
+            finger_turn_absolute_ = finger_value_
+
+        finger_turn_ = finger_turn_absolute_
+        finger_meter_ = [x * finger_maxDist / finger_maxTurn for x in finger_turn_]
+        finger_percent_ = [x / finger_maxTurn * 100.0 for x in finger_turn_]
+
+    elif unit_ == 'mm':
+        # get absolute value
+        finger_turn_command = [x/1000 * finger_maxTurn / finger_maxDist for x in finger_value_]
+        if relative_:
+            finger_turn_absolute_ = [finger_turn_command[i] + currentFingerPosition[i] for i in range(0, len(finger_value_))]
+        else:
+            finger_turn_absolute_ = finger_turn_command
+
+        finger_turn_ = finger_turn_absolute_
+        finger_meter_ = [x * finger_maxDist / finger_maxTurn for x in finger_turn_]
+        finger_percent_ = [x / finger_maxTurn * 100.0 for x in finger_turn_]
+    elif unit_ == 'percent':
+        # get absolute value
+        finger_turn_command = [x/100.0 * finger_maxTurn for x in finger_value_]
+        if relative_:
+            finger_turn_absolute_ = [finger_turn_command[i] + currentFingerPosition[i] for i in
+                                     range(0, len(finger_value_))]
+        else:
+            finger_turn_absolute_ = finger_turn_command
+
+        finger_turn_ = finger_turn_absolute_
+        finger_meter_ = [x * finger_maxDist / finger_maxTurn for x in finger_turn_]
+        finger_percent_ = [x / finger_maxTurn * 100.0 for x in finger_turn_]
+    else:
+        raise Exception("Finger value have to be in turn, mm or percent")
+
+    return finger_turn_, finger_meter_, finger_percent_
+
+
+def verboseParserFinger(verbose_, finger_turn_):
+    """ Argument verbose """
+    if verbose_:
+        finger_meter_ = [x * finger_maxDist / finger_maxTurn for x in finger_turn_]
+        finger_percent_ = [x / finger_maxTurn * 100.0 for x in finger_turn_]
+        print('Finger values in turn are: ')
+        print(', '.join('finger{:1.0f} {:4.0f}'.format(k[0] + 1, k[1]) for k in enumerate(finger_turn_)))
+        print('Finger values in mm are: ')
+        print(', '.join('finger{:1.0f} {:2.1f}'.format(k[0]+1, k[1]*1000) for k in enumerate(finger_meter_)))
+        print('Finger values in percentage are: ')
+        print(', '.join('finger{:1.1f} {:3.1f}%'.format(k[0]+1, k[1]) for k in enumerate(finger_percent_)))
+
+# GCode parsing begins here
 def gcodeParser():
-    f = open('obj2.gcode')
+    f = open('obj3.gcode')
     x = 0
     y = 0
     z = 0
@@ -265,6 +362,71 @@ def gcodeParser():
             path_list[i].append(0)
     return path_list
 
+def grab():
+    finger_turn, finger_meter, finger_percent = unitParserFinger('percent', [0, 0, 0], True)
+    try:
+        if finger_number == 0:
+            print('Finger number is 0, check with "-h" to see how to use this node.')
+            positions = []  # Get rid of static analysis warning that doesn't see the exit()
+            exit()
+        else:
+            positions_temp1 = [max(0.0, n) for n in finger_turn]
+            positions_temp2 = [min(n, finger_maxTurn) for n in positions_temp1]
+            positions = [float(n) for n in positions_temp2]
+
+        print('Sending finger position ...')
+        result = gripper_client(positions)
+        print('Finger position sent!')
+
+    except rospy.ROSInterruptException:
+        print('program interrupted before completion')
+
+    grabCartesian = [[0, 0, 0.2, 0, 0, -250], [0, 0, -0.2, 0, 0, 0]]
+    for i in grabCartesian:
+        pose_mq, pose_mdeg, pose_mrad = unitParserCartesian('mdeg', i, True)
+
+        try:
+
+            poses = [float(n) for n in pose_mq]
+
+            result = cartesian_pose_client(poses[:3], poses[3:])
+
+            print('Cartesian pose sent!')
+
+        except rospy.ROSInterruptException:
+            print "program interrupted before completion"
+
+    finger_turn, finger_meter, finger_percent = unitParserFinger('percent', [20, 20, 0], True)
+    try:
+        if finger_number == 0:
+            print('Finger number is 0, check with "-h" to see how to use this node.')
+            positions = []  # Get rid of static analysis warning that doesn't see the exit()
+            exit()
+        else:
+            positions_temp1 = [max(0.0, n) for n in finger_turn]
+            positions_temp2 = [min(n, finger_maxTurn) for n in positions_temp1]
+            positions = [float(n) for n in positions_temp2]
+
+        print('Sending finger position ...')
+        result = gripper_client(positions)
+        print('Finger position sent!')
+
+    except rospy.ROSInterruptException:
+        print('program interrupted before completion')
+
+    pose_mq, pose_mdeg, pose_mrad = unitParserCartesian('mdeg', [0, 0, 0.2, 0, 0, 0], True)
+
+    try:
+
+        poses = [float(n) for n in pose_mq]
+
+        result = cartesian_pose_client(poses[:3], poses[3:])
+
+        print('Cartesian pose sent!')
+
+    except rospy.ROSInterruptException:
+        print "program interrupted before completion"   
+
 
 if __name__ == '__main__':
 
@@ -287,14 +449,14 @@ if __name__ == '__main__':
 
     getcurrentCartesianCommand(prefix)
     a = gcodeParser()
-    print a[0:10]
+    # print a[0:10]
     temp = -0.1
-    test = [[temp, temp, 0, 0, 0, 70], [-0.2, -0.35, -0.40, 0, 0, 0]]
+    test = [[temp, temp, 0, 0, 0, 70], [-0.2, -0.35, -0.40, 0, 0, 0], [0, 0, -0.05, 0, 0, 0]]
     
 
     for i in test:
 
-        pose_mq, pose_mdeg, pose_mrad = unitParser('mdeg', i, True)
+        pose_mq, pose_mdeg, pose_mrad = unitParserCartesian('mdeg', i, True)
 
         try:
 
@@ -310,7 +472,7 @@ if __name__ == '__main__':
     # ser.write(b"r")
     for i in a:
 
-        pose_mq, pose_mdeg, pose_mrad = unitParser('mdeg', i, True)
+        pose_mq, pose_mdeg, pose_mrad = unitParserCartesian('mdeg', i, True)
 
         try:
 
@@ -324,6 +486,9 @@ if __name__ == '__main__':
             print "program interrupted before completion"
 
     # ser.write(b"s")
+
+    grab()
+
 
 
     
